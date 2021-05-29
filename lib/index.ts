2215dd3828ce0e4c6aa6d2a924b3eb04c5102b3f
Josua Blejeru <37913833+josuablejeru@users.noreply.github.com>
2021-05-30 01:02:17 +0200

    Initial commit

---
+++ lib/index.ts
@@ -0,0 +1,14 @@
+import * as cdk from '@aws-cdk/core';
+
+export interface AwsNextjsProps {
+  // Define construct properties here
+}
+
+export class AwsNextjs extends cdk.Construct {
+
+  constructor(scope: cdk.Construct, id: string, props: AwsNextjsProps = {}) {
+    super(scope, id);
+
+    // Define construct contents here
+  }
+}
