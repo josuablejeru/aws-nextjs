a723085a89904f6f3336f69e124f87b5fbdf1c83
Josua Blejeru <37913833+josuablejeru@users.noreply.github.com>
2021-06-01 20:04:30 +0300

    set the Construct with new resources: - s3 Bucket: basic assets - lambda for destribution - cloudfront distribution with s3 origin

--- lib/index.ts
+++ lib/index.ts
@@ -1,14 +1,61 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as s3 from "@aws-cdk/aws-s3";
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as cloudfront from "@aws-cdk/aws-cloudfront";
+import * as origin from "@aws-cdk/aws-cloudfront-origins";
 
 export interface AwsNextjsProps {
-  // Define construct properties here
+  frontendName: string;
+  /**
+   * configurations for the cloudfront distribution
+   * S3Origin is set by default
+   */
+  distributionProps?: cloudfront.DistributionProps;
 }
 
+/**
+ *
+ *
+ * To create the Edge function the us-east-1 stage must be bootstraped
+ */
 export class AwsNextjs extends cdk.Construct {
+  public readonly staticS3Bucket: s3.Bucket;
+  public readonly edgeProxy: cloudfront.experimental.EdgeFunction;
+  public readonly cloudfrontDistribution: cloudfront.Distribution;
 
-  constructor(scope: cdk.Construct, id: string, props: AwsNextjsProps = {}) {
+  constructor(scope: cdk.Construct, id: string, props: AwsNextjsProps) {
     super(scope, id);
 
-    // Define construct contents here
+    // the asset bucket
+    this.staticS3Bucket = new s3.Bucket(this, `${props.frontendName}`);
+
+    // the proxy function
+    this.edgeProxy = new cloudfront.experimental.EdgeFunction(
+      this,
+      "edge-proxy",
+      {
+        runtime: lambda.Runtime.NODEJS_4_3,
+        handler: "edge-proxy.handler",
+        code: lambda.Code.fromAsset("lambda"),
+      }
+    );
+
+    // the cloudfront Distribution with the edge function
+    this.cloudfrontDistribution = new cloudfront.Distribution(
+      this,
+      `${props.frontendName}-dist`,
+      {
+        defaultBehavior: {
+          origin: new origin.S3Origin(this.staticS3Bucket),
+          edgeLambdas: [
+            {
+              functionVersion: this.edgeProxy.currentVersion,
+              eventType: cloudfront.LambdaEdgeEventType.VIEWER_REQUEST,
+            },
+          ],
+        },
+        ...props.distributionProps,
+      }
+    );
   }
 }
