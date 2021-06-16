cc0460a0d705d6d93dc9eb61b05d8b797c8f61df
Josua Blejeru <37913833+josuablejeru@users.noreply.github.com>
2021-06-16 23:52:48 +0200

    create deployment from current repo from the repository

--- lib/index.ts
+++ lib/index.ts
@@ -1,12 +1,17 @@
 import * as cdk from "@aws-cdk/core";
 import * as s3 from "@aws-cdk/aws-s3";
+import * as s3deploy from "@aws-cdk/aws-s3-deployment";
 import * as lambda from "@aws-cdk/aws-lambda";
 import * as cloudfront from "@aws-cdk/aws-cloudfront";
 import * as origin from "@aws-cdk/aws-cloudfront-origins";
-import { resolve } from "path";
+import { relative, resolve } from "path";
 
 export interface AwsNextjsProps {
   frontendName: string;
+  /**
+   * path to directory there the asset of the frontend is stored
+   */
+  pathToFrontend: string;
   /**
    * configurations for the cloudfront distribution
    * S3Origin is set by default
@@ -15,12 +20,10 @@ export interface AwsNextjsProps {
 }
 
 /**
- *
- *
  * To create the Edge function the us-east-1 stage must be bootstraped
  */
 export class AwsNextjs extends cdk.Construct {
-  public readonly staticS3Bucket: s3.Bucket;
+  public readonly staticS3Bucket: s3.IBucket;
   public readonly edgeProxy: cloudfront.experimental.EdgeFunction;
   public readonly cloudfrontDistribution: cloudfront.Distribution;
 
@@ -29,13 +32,17 @@ export class AwsNextjs extends cdk.Construct {
 
     // the asset bucket
     this.staticS3Bucket = new s3.Bucket(this, `${props.frontendName}`);
+    new s3deploy.BucketDeployment(this, `FrontedSourceCode-${props.frontendName}`, {
+      sources: [s3deploy.Source.asset(props.pathToFrontend)],
+      destinationBucket: this.staticS3Bucket,
+    })
 
     // the proxy function
     this.edgeProxy = new cloudfront.experimental.EdgeFunction(
       this,
       "edge-proxy",
       {
-        runtime: lambda.Runtime.NODEJS_4_3,
+        runtime: lambda.Runtime.NODEJS_14_X,
         handler: "edge-proxy.handler",
         code: lambda.Code.fromAsset(`${resolve(__dirname)}/../lambda/`),
       }
